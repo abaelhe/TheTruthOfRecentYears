53d7d44c12127eac82a46af8c0d396bf665f10b8
Abael He abaelhe@icloud.com <abaelhe@icloud.com>
2025-09-14 03:17:20 +0800

    更新赔偿量化模型: 2025-09-13

--- pdf_to_one_image.py
+++ pdf_to_one_image.py
@@ -2,27 +2,28 @@ from io import StringIO, BytesIO
 from pdf2image import convert_from_path as cfp
 import os, sys, cv2 as cv, numpy as np
 
-pdffile = name_prefix = sys.argv[1]
 
 
-def mark_line(img, row_pos, col_pos, color, thickness):
-    print("  mark ", row_pos, col_pos)
-    t, (r0, r1), (c0, c1) = thickness, row_pos, col_pos
-    img[max(r0, 0) : (r0 + t), max(c0, 0) : c1, :] = color
-    img[max(r1 - t, 0) : r1, max(c0, 0) : c1, :] = color
-    img[max(r0, 0) : r1, max(c0, 0) : (c0 + t), :] = color
-    img[max(r0, 0) : r1, max(c1 - t, 0) : c1, :] = color
+def mark_line(img, bound_args, color, thickness):
+    print("  mark: %r " % (bound_args,))
+    t, (r0, r1, c0, c1) = thickness, bound_args
+    r0, r1, c0, c1 = max(r0, 0), r1,  max(c0, 0), c1
+    img[r0:(r0+t),       c0:c1,       :] = color
+    img[max(r1-t, 0):r1, c0:c1,       :] = color
+    img[r0:r1,           c0:(c0 + t), :] = color
+    img[r0:r1,  max(c1-t,0):c1,       :] = color
     return img
 
 
-def detect_row_bound(img, noise_threshold=10, min_pixels=3):
+def detect_bound(img, noise_threshold=10, min_pixels=3):
     rows, cols, chs = img.shape
-    head_index, tail_index = 0, (rows - 1)
-    total_bound = cols * chs * 255
+    row_s_max = cols * chs * 255
+    col_s_max = rows * chs * 255
     total_threshold = min_pixels * chs * noise_threshold
 
-    row_scores = [(total_bound - img[i, :, :].sum()) for i in range(rows)]
-
+    row_scores =[(row_s_max -img[i, :, :].sum()) for i in range(rows)]
+    col_scores =[(col_s_max -img[:, i, :].sum()) for i in range(cols)]
+    head_index, tail_index = 0, (rows - 1)
     hi, ti = head_index, tail_index
     while hi < rows:
         if row_scores[hi] > total_threshold:
@@ -36,14 +37,30 @@ def detect_row_bound(img, noise_threshold=10, min_pixels=3):
             break
         ti -= 1
 
-    return [head_index, tail_index]
+    left_index, right_index = 0, (cols - 1)
+    li, ri = left_index, right_index
+    while li < cols:
+        if col_scores[li] > total_threshold:
+            left_index = max(0, li - 1)
+            break
+        li += 1
+
+    while ri > 0:
+        if col_scores[ri] > total_threshold:
+            right_index = ri + 1
+            break
+        ri -= 1
+
+    bound_args = [head_index, tail_index, left_index, right_index]
+    print("crop bound: %s,\t%s,\t%s,\t%s" % (
+        head_index, tail_index, left_index, right_index))
+    return bound_args
 
 
 # image array processing [rows, columns, channels]
-def crop_image(img, row_pos, col_pos, chs_pos, noise_threshold=10):
-    cropped = img[
-        row_pos[0] : row_pos[1], col_pos[0] : col_pos[1], chs_pos[0] : chs_pos[1]
-    ]
+def crop_image(img, crop_args, chs_pos):
+    ca = crop_args
+    cropped = img[ ca[0]:ca[1], ca[2]:ca[3], chs_pos[0]:chs_pos[1] ]
     return cropped
 
 
@@ -57,6 +74,8 @@ def image_from_byte_io(bytes_io):
 
 def image_from_pdffile(pdf_file):
     # name_fmt = '%s.%d%s'
+    if not os.path.isfile(pdf_file):
+        raise SystemError("Doesn't exist or is not a file: %r" % pdf_file)
     images = cfp(pdf_file)
     images_num = len(images)
     bsios = [BytesIO() for i in range(images_num)]
@@ -65,8 +84,8 @@ def image_from_pdffile(pdf_file):
     return imgs, images_num
 
 
-def pdf_to_image0(pdf_file, noise_threshold=10, min_pixels=3, line_height=0):
-    imgs, images_num = image_from_pdffile(pdffile)
+def pdf_to_image(pdf_file, noise_threshold=10, min_pixels=3, line_height=0, width_ratio=1.00):
+    imgs, images_num = image_from_pdffile(pdf_file)
     if images_num < 1:
         return []
 
@@ -74,25 +93,40 @@ def pdf_to_image0(pdf_file, noise_threshold=10, min_pixels=3, line_height=0):
     rows, cols, chs = imgs[0].shape
     chs_pos, e_args, m_args = [0, chs], ((0, 0, 255), 2), (color, thickness)
     print("Cropping: rows:%s, cols:%s, " % (rows, cols))
+    
+    img_bounds, left_min, right_max =[], cols, 0
+    for i in range(len(imgs)):
+       bound = detect_bound(imgs[i], noise_threshold, min_pixels)
+       if bound[2] < left_min:
+           left_min = bound[2]
+       if bound[3] > right_max:
+           right_max = bound[3]
+       img_bounds.append(bound)
 
-    crop_col_bound = (0, cols)
+    width_margin = min(left_min, (cols - right_max))
+    width_margin = int(width_margin * width_ratio)
 
-    crop_row_head = detect_row_bound(imgs[0], noise_threshold, min_pixels)
-    crop_row_tail = detect_row_bound(imgs[-1], noise_threshold, min_pixels)
-    crop_row_tail = [
-        max(0, crop_row_tail[0] - line_height),
-        min(rows, crop_row_tail[1] + crop_row_head[0]),
-    ]
-    crop_row_head = [0, crop_row_head[1]]
+    crop_head = img_bounds[0]
+    crop_head[2] = width_margin
+    crop_head[3] = cols -1 - width_margin
 
-    crop_args = [(crop_row_head, crop_col_bound)]
+    crop_tail = img_bounds[-1]
+    crop_tail[0] = max(crop_tail[0] - line_height,     0)
+    crop_tail[1] = min(crop_tail[1] + crop_head[0], rows)
+    crop_tail[2], crop_tail[3] = crop_head[2], crop_head[3]
 
-    for i in imgs[1:-1]:
-        crop_row_bound = detect_row_bound(i, noise_threshold, min_pixels)
-        crop_row_bound = [max(0, crop_row_bound[0] - line_height), crop_row_bound[1]]
-        crop_args.append((crop_row_bound, crop_col_bound))
+    crop_head[0] = 0
 
-    crop_args.append((crop_row_tail, crop_col_bound))
+    crop_args = [crop_head]
+
+    for i in range(1, images_num-1):
+        crop_bound = img_bounds[i]
+        crop_bound[0] =max(0, crop_bound[0] - line_height)
+        crop_bound[1] =crop_bound[1]
+        crop_bound[2], crop_bound[3] = crop_head[2], crop_head[3]
+        crop_args.append(crop_bound)
+
+    crop_args.append(crop_tail)
 
     m_imgs = []
     i_imgs = []
@@ -100,13 +134,19 @@ def pdf_to_image0(pdf_file, noise_threshold=10, min_pixels=3, line_height=0):
     for i in range(len(imgs)):
         img = imgs[i]
         i_rows, i_cols, i_chs = img.shape
-
         crop_arg = crop_args[i]
-        m_img = mark_line(img.copy(), (0, i_rows), (0, i_cols), *e_args)
-        m_img = mark_line(m_img, *crop_arg, *m_args)
+        m_img = mark_line(img.copy(), (0, i_rows, 0, i_cols), *e_args)
         m_imgs.append(m_img)
 
-        i_img = crop_image(img.copy(), *crop_arg, [0, i_chs])
+    for i in range(len(imgs)):
+        m_img = m_imgs[i]
+        crop_arg = crop_args[i]
+        mark_line(m_img, crop_arg, *m_args)
+
+    for i in range(len(imgs)):
+        img = imgs[i]
+        crop_arg = crop_args[i]
+        i_img = crop_image(img, crop_arg, [0, i_chs])
         i_imgs.append(i_img)
 
     m_concated = cv.vconcat(m_imgs)
@@ -114,58 +154,20 @@ def pdf_to_image0(pdf_file, noise_threshold=10, min_pixels=3, line_height=0):
     return m_concated, i_concated
 
 
-def pdf_to_image1(pdf_file, row_ratio=0.0729, col_ratio=0.0):
-    imgs, images_num = image_from_pdffile(pdffile)
-    if images_num < 1:
-        return []
+def main():
+    if len(sys.argv) < 2:
+        raise SystemError("Usage: python  pdf_to_image.py <PDF_FILE>  | [JPEG_QUALITY]")
+    pdf_file = name_prefix = sys.argv[1]
+    jpg_quality=100
+    if len(sys.argv)> 2 and sys.argv[2].isdigit():
+        jpg_quality = int(sys.argv[2])
 
-    color, thickness = (255, 0, 0), 5
-    rows, cols, chs = imgs[0].shape
-    row_delta, col_delta = int(rows * row_ratio), int(cols * col_ratio)
-    chs_pos, e_args, m_args = [0, chs], ((0, 0, 255), 2), (color, thickness)
-    print("Cropping: rows:%s, cols:%s, " % (rows, cols))
+    # vertical concat all images in imgs_processed
+    m_concated, i_concated = pdf_to_image(
+        pdf_file, noise_threshold=10, min_pixels=3, line_height=0
+    )
+    cv.imwrite(pdf_file + "_marked.jpg", m_concated, (cv.IMWRITE_JPEG_QUALITY, jpg_quality))
+    cv.imwrite(pdf_file + ".jpg", i_concated, (cv.IMWRITE_JPEG_QUALITY, jpg_quality))
 
-    crop_arg_head = ((0, rows - row_delta), (col_delta, cols - col_delta))
-    crop_arg_other = ((row_delta, rows - row_delta), (col_delta, cols - col_delta))
-    crop_arg_tail = ((row_delta, rows), (col_delta, cols - col_delta))
-
-    # import pdb;pdb.set_trace()
-    m_concated, i_concated = None, None
-    if images_num == 1:
-        m_head = mark_line(imgs[0].copy(), (0, rows), (0, cols), *e_args)
-        m_head = mark_line(m_head, *crop_arg_other, *m_args)
-        m_concated = m_head
-        i_head = crop_image(imgs[0].copy(), (0, rows), (0, cols), chs_pos)
-        i_concated = i_head
-    elif images_num > 1:
-        m_head = mark_line(imgs[0].copy(), (0, rows), (0, cols), *e_args)
-        m_head = mark_line(m_head, *crop_arg_head, *m_args)
-        m_other = [
-            mark_line(i.copy(), (0, rows), (0, cols), *e_args) for i in imgs[1:-1]
-        ]
-        m_other = [mark_line(i, *crop_arg_other, *m_args) for i in m_other]
-        m_tail = mark_line(imgs[-1].copy(), (0, rows), (0, cols), *e_args)
-        m_tail = mark_line(m_tail, *crop_arg_tail, *m_args)
-        m_imgs = [m_head] + m_other + [m_tail]
-        m_concated = cv.vconcat(m_imgs)
-
-        i_head = crop_image(imgs[0].copy(), *crop_arg_head, chs_pos)
-        i_other = [crop_image(i.copy(), *crop_arg_other, chs_pos) for i in imgs[1:-1]]
-        i_tail = crop_image(imgs[-1].copy(), *crop_arg_tail, chs_pos)
-        i_imgs = [i_head] + i_other + [i_tail]
-        i_concated = cv.vconcat(i_imgs)
-    return m_concated, i_concated
-
-
-# vertical concat all images in imgs_processed
-m_concated, i_concated = pdf_to_image0(
-    pdffile, noise_threshold=10, min_pixels=3, line_height=0
-)
-cv.imwrite(pdffile + "_0marked.jpg", m_concated)
-cv.imwrite(pdffile + "_0.jpg", i_concated)
-
-m_concated, i_concated = pdf_to_image1(pdffile, row_ratio=0.0729, col_ratio=0.0)
-
-# save the processed image array
-cv.imwrite(pdffile + "_1marked.jpg", m_concated)
-cv.imwrite(pdffile + "_1.jpg", i_concated)
+if "__main__" == __name__:
+    main()
